373e60a91cf2d74d0c33b6f7953ca280b80dac74
youngsun <sunnyskylove22@gmail.com>
2024-02-01 17:44:43 +0900

    [test_정리후 수정] 연산놀이 변수,상수,테스트

---
+++ src/main/java/com/ohgiraffers/section02/variable/Teamtest.java
@@ -0,0 +1,35 @@
+package com.ohgiraffers.section02.variable;
+
+public class Teamtest {
+    public static void main(String[] args) {
+
+        System.out.println('d' + ( 'a' * 2));
+
+//        int age = 10
+          int point =100;
+//        System.out.println("포인트 10프로가 차감되었습니다."+ point-(point*0.1));
+
+        //*문제 풀이*//
+        System.out.println("화이팅!!:" + 'd' +( 'a' * 2));
+        System.out.println("잘가!!:" + ('d' + 'a' * 2));
+
+        /*boolean 만들기*/
+        int drum = 10;
+        System.out.println("drum의 개수는?:" + 5*2);
+//
+
+
+        //* 오류내용 찾기!! *//
+//        int sum = 50;
+//        System.out.println(sum+22);
+//        System.out.println(sum-10);
+//
+//        int sum = 20;
+//        System.out.println("처음의 sum:"+sum);
+//        sum = sum + 10;
+//        System.out.println("2번째의 sum:"+ sum);
+
+
+    }
+
+}
